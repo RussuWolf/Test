9e69ca7f13e97f0ebe01fd0521e2ea8866ea021f
Russu Peter <russu.pw@gmail.com>
2017-07-28 22:26:42 +0300

    was corrected

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#burger-container").on('click', function(){
         $(this).toggleClass("open");
-        $("nav").slideToggle(400, function() {
+        $("nav").slideToggle(200, function() {
 			$(this).toggleClass("nav-expanded").css('display', '');
 		});
   		//$(".header_content").owlCarousel();
